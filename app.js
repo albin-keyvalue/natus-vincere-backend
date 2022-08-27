8f937fc4f97d538773acf0113330968fcdf21400
Albin Kottaram <albin@keyvalue.systems>
2022-08-28 00:07:45 +0530

    port change and bug fix

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const service = require('./service/index.js');
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 9090
 
 
 app.get('/combinations', async (req, res) => {
 	var response = null
 	if (req.query.type == 'top') 
 		response = await service.getTopRecommendation(req.query.color, req.query.gender)
-	else if (type == 'bottom')
+	else if (req.query.type == 'bottom')
 		response = await service.getBottomRecommendation(req.query.color, req.query.gender)
-	else if (type == 'shoes')
+	else if (req.query.type == 'shoes')
 		response = await service.getShoeRecommendation(req.query.color, req.query.gender)
 	res.send(response)
 })
